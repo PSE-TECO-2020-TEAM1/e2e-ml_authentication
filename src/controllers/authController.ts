2d592f87860cf22c3061be205b23697126a830aa
Mustafa Enes Batur <thifhi99@gmail.com>
2021-01-28 20:25:52 +0300

    Add controllers

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,30 +1,84 @@
 import { Request, Response, NextFunction } from "express"
-import {User, IUser} from "../models/user"
+import User from "../models/user"
 import bcrypt from "bcrypt"
-import { NativeError } from "mongoose";
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
 
-interface SignupRequest extends Request {
+const TOKEN_DURATION = 15 * 60 * 1000;
+
+interface SignupRequestBody {
     email: string
     username: string
     password: string
 }
 
-export const postSignup = async (req: SignupRequest, res: Response, next: NextFunction) => {
-    console.log(req.body);
-    console.log(req.body.email);
-    User.findOne({ email: req.body.email }, (err: NativeError, user: IUser) => {
-        if (err) {
-            
-        }
-        if (user) {
-            res.status(500).json({error: "A user with the given email already exists."});
-        }
-    });
-    const passwordHash = await bcrypt.hash(req.body.password, 10);
-    await User.create({ email: req.body.email, username: req.body.username, passwordHash: passwordHash });
+export const postSignup = async (req: Request<{}, {}, SignupRequestBody>, res: Response, next: NextFunction) => {
+    const body: SignupRequestBody = req.body;
+    // Check if the email is unique
+    let user = await User.findOne({ email: body.email }).exec();
+    if (user) {
+        return res.status(400).send("A user with the given email already exists");
+    }
+    // Check if the username is unique 
+    user = await User.findOne({username: body.username }).exec();
+    if (user) {
+        return res.status(400).send("A user with the given username already exists");
+    }
+    // Create new user
+    const passwordHash = await bcrypt.hash(body.password, 10);
+    await User.create({ email: body.email, username: body.username, passwordHash: passwordHash });
     res.sendStatus(200);
 }
 
-export const postLogin = async (req: Request, res: Response, next: NextFunction) => {
-
+interface LoginRequestBody {
+    username: string
+    password: string
 }
+
+interface LoginResponseBody {
+    accessToken: string,
+    refreshToken: string
+}
+
+export const postLogin = async (req: Request, res: Response, next: NextFunction) => {
+    const body: LoginRequestBody = req.body;
+    const user = await User.findOne({ username: body.username }).exec();
+    if (!user) {
+        return res.status(400).send("A user with the given username doesn't exist");
+    }
+    if (!await bcrypt.compare(body.password, user.passwordHash)) {
+        return res.status(400).send("Wrong password");
+    }
+    
+    const payload = {
+        username: user.username,
+        expiration: Date.now() + TOKEN_DURATION
+    }
+    // Create the access and refresh tokens
+    const accessToken = jwt.sign(payload, "sabahci_kahvesi");
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+    user.accessToken = accessToken;
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    const responseJson: LoginResponseBody = { accessToken, refreshToken };
+    res.status(200).json(responseJson);
+}
+
+interface RefreshRequestBody {
+    refreshToken: string
+}
+
+export const postRefresh = async (req: Request, res: Response, next: NextFunction) => {
+    const body: RefreshRequestBody = req.body;
+    const user = await User.findOne({ refreshToken: body.refreshToken }).exec();
+    if (!user) {
+        return res.status(400).send("Invalid refresh token");
+    }
+       
+}
+
+function createNewTokens(username: string): {accessToken: string, refreshToken: string} {
+    
+}
+
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,31 +1,39 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
 
-export interface IUser extends mongoose.Document {
+interface IUser extends mongoose.Document {
     email: string,
     email_verified: boolean,
     username: string,
     passwordHash: string,
+    accessToken: string,
+    refreshToken: string
 }
 
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     email_verified: {
         type: Boolean,
-        default: false,
+        default: false
     },
     username: {
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     passwordHash: {
         type: String,
-        required: true,
+        required: true
+    },
+    accessToken: {
+        type: String
+    },
+    refreshToken: {
+        type: String
     }
 });
 
@@ -34,4 +42,5 @@ UserSchema.methods.isValidPassword = async function (this: IUser, password: stri
     return compare;
 }
 
-export const User = mongoose.model<IUser>("User", UserSchema);
+const User = mongoose.model<IUser>("User", UserSchema);
+export default User
